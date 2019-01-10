f59b359dab3687dd456c1d15c9430a8d1e943cf9
yannsheriff <yanncherif@live.fr>
2019-01-10 23:09:10 +0100

    fix time display

--- src/components/trainingList.js
+++ src/components/trainingList.js
@@ -17,11 +17,7 @@ export default class trainingList extends Component {
       var hours   = Math.floor(sec_num / 3600);
       var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
       var seconds = sec_num - (hours * 3600) - (minutes * 60);
-
-      if (hours   < 10) {hours   = "0"+hours;}
-      if (minutes < 10) {minutes = "0"+minutes;}
-      if (seconds < 10) {seconds = "0"+seconds;}
-
+      
       hours = hours > 0 ? hours+'h ' : ''
       minutes = minutes > 0 ? minutes+'m ' : ''
       seconds = seconds > 0 ? seconds+'s ' : ''
