07027a222625d53975c7c522ff617abc0886de3f
yannsheriff <yanncherif@live.fr>
2019-03-07 18:22:15 +0100

    [refacto] stepList

---
+++ app/components/stepList/style.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+import screen from '../../helpers/ScreenSize';
+
+export default StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    paddingTop: 20
+  },
+  step: {
+    height: 50, width: screen.widthPercent * 80, borderRadius: 15, backgroundColor: 'green', justifyContent: 'center', alignItems: 'center', marginBottom: 10
+  },
+});
