018c0a889a2ff33d35ce0780c1853c8e38793d90
yannsheriff <yanncherif@live.fr>
2019-03-13 21:05:35 +0100

    create new data from schema

---
+++ app/helpers/Migrator.js
@@ -0,0 +1,37 @@
+export function compare(schema, object) {
+  const schemaKeys = Object.keys(schema);
+  const objectKeys = Object.keys(object);
+  const keymap = schemaKeys.map((schemaKey) => {
+    const keyExist = objectKeys.find(objectKey => schemaKey === objectKey);
+    if (keyExist) {
+      return false;
+    }
+    return true;
+  });
+  const haveError = keymap.find(element => element);
+  if (haveError) {
+    return false;
+  }
+  return true;
+}
+
+export function findMissingKey(schema, object) {
+  const schemaKeys = Object.keys(schema);
+  const objectKeys = Object.keys(object);
+  const keymap = schemaKeys.filter((schemaKey) => {
+    const keyExist = objectKeys.find(objectKey => schemaKey === objectKey);
+    if (keyExist === undefined) {
+      return schemaKey;
+    }
+  });
+  return keymap;
+}
+
+export function migrate(schema, object) {
+  const missingKey = findMissingKey(schema, object);
+  const newObject = { ...object };
+  missingKey.forEach((key) => {
+    newObject[key] = schema[key];
+  });
+  return newObject;
+}
--- app/helpers/storeService.js
+++ app/helpers/storeService.js
@@ -1,10 +1,13 @@
+/* eslint-disable class-methods-use-this */
 
 import { AsyncStorage } from 'react-native';
+import { compare, migrate } from './Migrator';
+import schema, { training, phases, steps } from '../config/schema';
 
 let instance = null;
 class StoreService {
   constructor() {
-    if(!instance){
+    if (!instance) {
       instance = this;
     }
     return instance;
@@ -18,22 +21,19 @@ class StoreService {
       throw error;
     }
   }
-  
-  async getSaving() {
 
-    let dataSaved = await AsyncStorage.getItem("saved");
-    console.log(dataSaved)
+  async getSaving() {
+    const dataSaved = await AsyncStorage.getItem('saved');
     if (dataSaved !== null && dataSaved) {
-        return JSON.parse(dataSaved)
-    } else {
-        return false
+      return JSON.parse(dataSaved);
     }
+    return false;
   }
-  
+
   async save(selectedValue) {
     try {
-      let jsonValue = JSON.stringify(selectedValue);
-      return await AsyncStorage.setItem("saved", jsonValue);
+      const jsonValue = JSON.stringify(selectedValue);
+      return await AsyncStorage.setItem('saved', jsonValue);
     } catch (error) {
       throw error;
     }
@@ -41,7 +41,7 @@ class StoreService {
 
   async set(itemKey, selectedValue) {
     try {
-      let jsonValue = JSON.stringify(selectedValue);
+      const jsonValue = JSON.stringify(selectedValue);
       return await AsyncStorage.setItem(itemKey, jsonValue);
     } catch (error) {
       throw error;
@@ -55,45 +55,91 @@ class StoreService {
       throw error;
     }
   }
-  
-  async loadState() { //  initial state
-      const dataSaved = await this.getSaving()
-      if (dataSaved) {
-        console.log(' ================ Async Storage ================ ')
-        console.log("last Save :", dataSaved )
-        return dataSaved
-      } else {
-        
-        var state = [{
-            name : 'training',
-            phases : [
-                {
-                    name : 'phase 1',
-                    repetitions : 1,
-                    steps : [
-                        {
-                            name : 'planche',
-                            duration : 7, 
-                            key: 's-6789'
-                        },
-                        {
-                            name : 'pause',
-                            duration : 5,
-                            key: 's-6788'
-                        },
-                        {
-                            name : 'planche',
-                            duration : 10,
-                            key: 's-6787'
-                        },
-                    ]
-                }
-            ]
-        }]
 
-        return state
-      }
+  compareSchema(data) {
+    const trainingData = data[0];
+    const phaseData = trainingData.phases[0];
+    const stepData = phaseData.steps[0];
+
+    const trainingIsConsistent = compare(training, trainingData);
+    const phaseIsConsistent = compare(phases, phaseData);
+    const stepIsConsistent = compare(steps, stepData);
+
+    const result = [trainingIsConsistent, phaseIsConsistent, stepIsConsistent];
+
+    return result;
+  }
+
+
+  migrateSchema(dataSaved) {
+    const migrationMap = this.compareSchema(dataSaved);
+    const migrateTraining = migrationMap[0];
+    const migratePhase = migrationMap[1];
+    const migrateStep = migrationMap[2];
+
+    let data = [...dataSaved];
+    if (!migrateTraining) {
+      data = dataSaved.map(trainingData => migrate(training, trainingData));
+    }
+    if (!migratePhase) {
+      data = data.map(trainingData => ({
+        ...trainingData,
+        phases: trainingData.phases.map(phaseData => migrate(phases, phaseData))
+      }));
+    }
+    if (!migrateStep) {
+      data = data.map(trainingData => ({
+        ...trainingData,
+        phases: trainingData.phases.map(phaseData => ({
+          ...phaseData,
+          steps: phaseData.steps.map(stepsData => migrate(steps, stepsData))
+        }))
+      }));
+    }
+
+    console.log('TCL: StoreService -> migrateSchema -> data', data);
+  }
+
+  async loadState() { //  initial state
+    const dataSaved = await this.getSaving();
+    if (dataSaved) {
+      console.log(' ================ Async Storage ================ ');
+      console.log('last Save :', dataSaved);
+      this.migrateSchema(dataSaved);
+
+
+      return dataSaved;
+    }
+
+    const state = [{
+      name: 'training',
+      phases: [
+        {
+          name: 'phase 1',
+          repetitions: 1,
+          steps: [
+            {
+              name: 'planche',
+              duration: 7,
+              key: 's-6789'
+            },
+            {
+              name: 'pause',
+              duration: 5,
+              key: 's-6788'
+            },
+            {
+              name: 'planche',
+              duration: 10,
+              key: 's-6787'
+            },
+          ]
+        }
+      ]
+    }];
+
+    return state;
   }
 }
 
-export let storeService = new StoreService();
+export const storeService = new StoreService();
