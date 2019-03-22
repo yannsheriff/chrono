b9e5770c1ac2a3b3982cf20b6b444b9fcc4272ad
yannsheriff <yanncherif@live.fr>
2019-03-22 22:15:35 +0100

    add reactnative svg

--- android/app/src/main/java/com/chrono/MainApplication.java
+++ android/app/src/main/java/com/chrono/MainApplication.java
@@ -3,6 +3,7 @@ package com.chrono;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.wix.interactable.Interactable;
 import com.corbt.keepawake.KCKeepAwakePackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SvgPackage(),
             new LinearGradientPackage(),
             new Interactable(),
             new KCKeepAwakePackage(),
