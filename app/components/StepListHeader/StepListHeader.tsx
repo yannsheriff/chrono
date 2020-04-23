1fd9f93122b23560ebf9a18e9047e65a4b672e24
Yannis Cherif <yanncherif@live.fr>
2020-04-23 17:23:57 +0200

    type stepListHeader

---
+++ app/components/StepListHeader/StepListHeader.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './style';
+import { chrono } from '../../helpers/humanize';
+import ProgressBar from '../ProgressBar';
+
+interface Props {
+  name: string;
+  doneTime: number;
+  totalTime: number;
+}
+
+const ChronoRemote: React.FunctionComponent<Props> = ({
+  name,
+  doneTime,
+  totalTime,
+}) => (
+  <View style={styles.container}>
+    <View style={styles.wrapper}>
+      <Text style={styles.name}>{name} </Text>
+      <View style={styles.timeContainer}>
+        <Text style={styles.doneTime}>{chrono(doneTime)} </Text>
+        <Text style={styles.time}>/ {chrono(totalTime)}</Text>
+      </View>
+      <ProgressBar progress={doneTime * (100 / totalTime)} />
+    </View>
+  </View>
+);
+
+export default ChronoRemote;
